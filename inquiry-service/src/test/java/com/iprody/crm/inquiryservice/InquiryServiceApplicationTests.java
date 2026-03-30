5b6579c250ee66efbab1a3a0c26f1e944bdda3bd
dmitriev <dmitriev@protei.ru>
2026-03-30 17:16:51 +0300

    Payment Service:add payment-service, README.md, docker-compose.yml

---
+++ inquiry-service/src/test/java/com/iprody/crm/inquiryservice/InquiryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.iprody.crm.inquiryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class InquiryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
