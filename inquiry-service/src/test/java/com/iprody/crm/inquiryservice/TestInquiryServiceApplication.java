999aa3ababc3a29558b48d0a871779342c896af7
dmitriev <dmitriev@protei.ru>
2026-04-05 19:10:35 +0300

    establish infrastructure

--- inquiry-service/src/test/java/com/iprody/crm/inquiryservice/TestInquiryServiceApplication.java
+++ inquiry-service/src/test/java/com/iprody/crm/inquiryservice/TestInquiryServiceApplication.java
@@ -1,11 +1,11 @@
-package com.iprody.crm.inquiryservice;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestInquiryServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.from(InquiryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
-}
+//package com.iprody.crm.inquiryservice;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestInquiryServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.from(InquiryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+//	}
+//
+//}
--- payment-service/src/test/java/com/iprody/crm/paymentservice/TestPaymentServiceApplication.java
+++ payment-service/src/test/java/com/iprody/crm/paymentservice/TestPaymentServiceApplication.java
@@ -1,11 +1,11 @@
-package com.iprody.crm.paymentservice;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestPaymentServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.from(PaymentServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
-}
+//package com.iprody.crm.paymentservice;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestPaymentServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.from(PaymentServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+//	}
+//
+//}
